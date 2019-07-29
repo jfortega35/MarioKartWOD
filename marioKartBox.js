4f63d7bf09bf4d8a2f9df17e2f3dd04922dabc4c
Jacinto Ortega <jfortega35@Jacintos-MacBook-Pro.local>
2019-07-29 12:07:30 -0400

    modified css and js to fit item box

--- marioKartBox.js
+++ marioKartBox.js
@@ -3,55 +3,53 @@ let boxResult = document.getElementById("boxResult")
 var info = true;
 function doSpin(){
 	if (doing){
-        return null;
-    }
+        return null;}
 	doing = true;
 	var numChanges = randomInt(1,4)*8
     var numberitemBox = numChanges + randomInt(1,8)
-	var i1 = 0;
-	boxResult.innerHTML = "SPINNING";
+	var i = 0;
+	boxResult.innerHTML = "SPINNING..."
 	itemBox = setInterval(spin, 50);
 	function spin(){
-		i1++;
-		if (i1>=numberitemBox){
+		i++;
+		if (i>=numberitemBox){
             clearInterval(itemBox);
             getPenalty();
 			return null;
 		}
-		slotTile = document.getElementById("itemBox");
-		if (slotTile.className=="a8"){
-			slotTile.className = "a0";
+		itemTile = document.getElementById("itemBox");
+		if (itemTile.className=="a8"){
+			itemTile.className = "a0";
 		}
-		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
+        itemTile.className = "a"+(parseInt(itemTile.className.substring(1))+1);
 	}
 }
 function getPenalty(){
 	var itemBox = document.getElementById("itemBox").className
-    
     switch (itemBox){
         case "a1":
-            boxResult.innerHTML = "You Do 10 Pull-Ups";
+            boxResult.innerHTML = "YOU DO 10 PULL-UPS";
             break;
         case "a2":
-            boxResult.innerHTML = "First Place, Run 200 meters";
+            boxResult.innerHTML = "FIRST PLACE, RUN 200 METERS";
             break;
         case "a3":
-            boxResult.innerHTML = "You Do 10 Toe-to-Bar";
+            boxResult.innerHTML = "YOU DO 10 TOE-TO-BAR";
             break;
         case "a4":
-            boxResult.innerHTML = "Person to Your Left, Do 15 KB Swings";
+            boxResult.innerHTML = "PERSON TO YOUR LEFT, DO 15 KB SWINGS";
             break;
         case "a5":
-            boxResult.innerHTML = "Everyone but you, Do 15 Burpees";
+            boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BURPEES";
             break;
         case "a6":
-            boxResult.innerHTML = "Everyone but you, Do 15 Sit-Ups";
+            boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 SIT-UPS";
             break;
         case "a7":
-            boxResult.innerHTML = "Pick Someone to do 15 KB Swings";
+            boxResult.innerHTML = "PICK SOMEONE TO DO 15 KB SWINGS";
             break;
         case "a8":
-            boxResult.innerHTML = "Everyone but you, Do 15 Box Jumps";
+            boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BOX JUMPS";
             break;           
     }
 	doing = false;
