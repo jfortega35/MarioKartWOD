8a2dd96e270b99d4f53d9a3697d858ed9009fb1a
Jacinto Ortega <jfortega35@Jacintos-MacBook-Pro.local>
2019-07-30 19:44:51 -0400

    added audio

--- wodioKartBox.js
+++ wodioKartBox.js
@@ -1,10 +1,12 @@
 var doing = false;
 let boxResult = document.getElementById("boxResult")
 var info = true;
+var itemAudio = new sound("audio/starPower.mp3");
+
 function doSpin(){
 	if (doing){
         return null;}
-	doing = true;
+    doing = true;
 	var numChanges = randomInt(1,4)*8
     var numberitemBox = numChanges + randomInt(1,8)
 	var i = 0;
@@ -29,27 +31,35 @@ function getPenalty(){
     switch (itemBox){
         case "a1":
             boxResult.innerHTML = "YOU DO 10 PULL-UPS";
+            itemAudio.stop();
             break;
         case "a2":
             boxResult.innerHTML = "FIRST PLACE, RUN 200 METERS";
+            itemAudio.stop();
             break;
         case "a3":
             boxResult.innerHTML = "YOU DO 10 TOE-TO-BAR";
+            itemAudio.stop();
             break;
         case "a4":
             boxResult.innerHTML = "PERSON TO YOUR LEFT, DO 15 KB SWINGS";
+            itemAudio.stop();
             break;
         case "a5":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BURPEES";
+            itemAudio.stop();
             break;
         case "a6":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 SIT-UPS";
+            itemAudio.stop();
             break;
         case "a7":
             boxResult.innerHTML = "PICK SOMEONE TO DO 15 KB SWINGS";
+            itemAudio.stop();
             break;
         case "a8":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BOX JUMPS";
+            itemAudio.play();
             break;           
     }
 	doing = false;
@@ -58,3 +68,18 @@ function getPenalty(){
 function randomInt(min, max){
 	return Math.floor((Math.random() * (max-min+1)) + min);
 }
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
