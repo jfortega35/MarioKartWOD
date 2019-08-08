41b2b3ccd36856be94b0edfd17014f09f976329c
Jacinto Ortega <jfortega35@Jacintos-MacBook-Pro.local>
2019-08-08 00:24:58 -0400

    added bananaDrop audio file

--- wodioKartBox.js
+++ wodioKartBox.js
@@ -1,7 +1,8 @@
 var doing = false;
 let boxResult = document.getElementById("boxResult")
 var info = true;
-var itemAudio = new sound("audio/starPower.mp3");
+var starAudio = new sound("audio/starPower.mp3");
+var lightningAudio = new sound("audio/lightning.mp3");
 
 function doSpin(){
 	if (doing){
@@ -31,35 +32,36 @@ function getPenalty(){
     switch (itemBox){
         case "a1":
             boxResult.innerHTML = "YOU DO 10 PULL-UPS";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a2":
             boxResult.innerHTML = "FIRST PLACE, RUN 200 METERS";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a3":
             boxResult.innerHTML = "YOU DO 10 TOE-TO-BAR";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a4":
             boxResult.innerHTML = "PERSON TO YOUR LEFT, DO 15 KB SWINGS";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a5":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BURPEES";
-            itemAudio.stop();
+            starAudio.stop();
+            lightningAudio.play();
             break;
         case "a6":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 SIT-UPS";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a7":
             boxResult.innerHTML = "PICK SOMEONE TO DO 15 KB SWINGS";
-            itemAudio.stop();
+            starAudio.stop();
             break;
         case "a8":
             boxResult.innerHTML = "EVERYONE BUT YOU, DO 15 BOX JUMPS";
-            itemAudio.play();
+            starAudio.play();
             break;           
     }
 	doing = false;
